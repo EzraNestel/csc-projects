11bc41ca6cc31ac5915a6dc212a7fdb5218734dd
Ezra Nestel <ezranestel@gmail.com>
2018-10-16 18:59:02 -0700

    Finish Assignment 4 Part IV Removed c file used to copy over hint section

--- Playground/main.c
+++ Playground/main.c
@@ -1,6 +1,15 @@
+
+#include <stdlib.h>
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+
+int main(void) {
+    FILE* notPron;
+    notPron = fopen("/Users/ezranestel/Documents/GitHub/csc-projects/Playground/not.html","w");
+    fprintf(notPron, "<html> \n");
+    fprintf(notPron, " <h3> YES </h3> \n");
+    fprintf(notPron, "</html> \n");
+    fclose("not.html");
+    return EXIT_SUCCESS;
 }
