2e99dcdc40d996847ccebd81b0691ffae827943f
Ezra Nestel <ezranestel@gmail.com>
2018-10-17 23:04:11 -0700

    Finish Assignment 4 Part IV Removed c file used to copy over hint section

---
+++ Playground/cmake-build-debug/CMakeFiles/Playground.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Playground.dir/main.c.o"
+  "Playground.pdb"
+  "Playground"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Playground.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
