792d955b03d4409a3dc10b515fb75a0b38eaae94
Ezra Nestel <ezranestel@gmail.com>
2018-10-22 16:56:04 -0700

    Updated playground for lab work assignment

--- Playground/main.c
+++ Playground/main.c
@@ -5,11 +5,6 @@
 
 
 int main(void) {
-    FILE* notPron;
-    notPron = fopen("/Users/ezranestel/Documents/GitHub/csc-projects/Playground/not.html","w");
-    fprintf(notPron, "<html> \n");
-    fprintf(notPron, " <h3> YES </h3> \n");
-    fprintf(notPron, "</html> \n");
-    fclose("not.html");
+    
     return EXIT_SUCCESS;
 }
