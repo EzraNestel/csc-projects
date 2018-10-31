6d5f2f7d33d4be2963305ae0e7ad31dae51c9a70
Ezra Nestel <ezranestel@gmail.com>
2018-10-31 15:01:15 -0700

    Create Assignment 5 Part 4 finished...

--- V00906949A5P4/main.c
+++ V00906949A5P4/main.c
@@ -2,10 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-// helper to convert a value into a string
-#define STR_HELPER(x) #x
-#define STR(x) STR_HELPER(x)
-
 #define ID0 ("")
 #define ID1 ("   ")
 #define ID2 ("     ")
@@ -20,6 +16,10 @@
 #define WINWIDTH (800)
 #define WINHEIGHT (320)
 
+// helper macro to convert a value into a string
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
 #define MAXNUMSHAPES 1000
 #define STRSH (STR(MAXNUMSHAPES) " shapes generated")
 
@@ -31,7 +31,14 @@
 #define YMAX (WINHEIGHT)
 #define RADMIN (10)
 #define RADMAX (50)
-
+#define RXMIN (10)
+#define RXMAX (30)
+#define RYMIN (10)
+#define RYMAX (30)
+#define WMIN (10)
+#define WMAX (40)
+#define HMIN (10)
+#define HMAX (40)
 #define FALLSHADE (1)
 #define REDSHADE (2)
 #define GREENSHADE (3)
@@ -39,7 +46,7 @@
 #define PURPLESHADE (5)
 #define FAVORITESHADE (6)
 
-#define SHADE (FALLSHADE)
+#define SHADE (FAVORITESHADE)
 
 #if SHADE == FALLSHADE
 #define REDMIN (100)
@@ -91,7 +98,7 @@
 
 //prototypes
 int main(void);
-int prng(int iLow, int iHigh);
+int prngInt(int iLow, int iHigh);
 float prngFloat(float fLow, float fHigh, float precision);
 FILE* createFile(char* fnam);
 void prologue(FILE* ofp, char* winTabTitle, char* opening, char* col);
@@ -103,21 +110,24 @@ void closeSVG(FILE* ofp, char* id);
 void commentSVG(FILE* ofp, char* id, char* cmt);
 void fillRectSVG(FILE* ofp, char* id, int x, int y, int w, int h, int r, int g, int b, float op);
 void fillCircleSVG(FILE* ofp, char* id, int cx, int cy, int rad, int r, int g, int b, float op);
+void fillEllipseSVG(FILE* ofp, char* id, int cx, int cy, int rx, int ry, int r, int g, int b, float op);
 void textSwipeAnimateSVG(FILE* ofp, char* id, char* txt, int fsz, int x, int y, int r, int g, int b, float op);
-void textSwipeAnimateSVG(FILE* ofp, char* id, char* txt, int fsz, int x, int y, int r, int g, int b, float op);
-void genRandomArtNumSet(int *x, int *y, int *rad,
-                        int *r, int *g, int *b, float *op);
+void genRandomArtNumSet(int *sh, int *x, int *y, int *rad, int *rx, int *ry, int *w, int *h, int *r, int *g, int *b, float *op) ;
 void genRandomArt(FILE *ofp, char *id, int nShapes);
 
 int main(void){
     FILE *ofp = createFile(FILENAME);
-    prologue(ofp, TABTITLE, WELCOME, "red");
-    genRandomArt(ofp, ID2, MAXNUMSHAPES);
-    epilogue(ofp, RESUME, "blue");
+    if (ofp == NULL){
+        printf("createFile function not written yet\n");
+    } else {
+        prologue(ofp, TABTITLE, WELCOME, "red");
+        genRandomArt(ofp, ID2, MAXNUMSHAPES);
+        epilogue(ofp, RESUME, "blue");
+    }//if
     return EXIT_SUCCESS;
 }//main
 
-int prng(int iLow, int iHigh) {
+int prngInt(int iLow, int iHigh) {
     int diff = (iHigh - iLow) + 1; // + 1 fixes rand()%MAX-1 issue
     return rand()%diff + iLow;
 }//prngInt
@@ -130,20 +140,28 @@ float prngFloat(float fLow, float fHigh, float precision) {
     return (float)(random)/precision;
 }//prngFloat
 
-void genRandomArtNumSet(int *x, int *y, int *rad, int *r, int *g, int *b, float *op) {
-    *x = *y = *rad = *r = *g = *b = 0;
+
+void genRandomArtNumSet(int *sh, int *x, int *y, int *rad, int *rx, int *ry, int *w, int *h, int *r, int *g, int *b, float *op) {
+
+    *sh = *x = *y = *rad = *rx = *ry = *w = *h = *r = *g = *b = 0;
     *op = 1.0;
 
-    *x = prng(XMIN,XMAX);
-    *y = prng(YMIN,YMAX);
-    *rad = prng(RADMIN,RADMAX);
-    *r = prng(REDMIN,REDMAX);
-    *g = prng(GREENMIN,GREENMAX);
-    *b = prng(BLUEMIN, BLUEMAX);
+    *sh = prngInt(SHAPEMIN,SHAPEMAX);
+    *x = prngInt(XMIN,XMAX);
+    *y = prngInt(YMIN,YMAX);
+    *rad = prngInt(RADMIN,RADMAX);
+    *rx = prngInt(RXMIN,RXMAX);
+    *ry = prngInt(RYMIN,RYMAX);
+    * w = prngInt(WMIN,WMAX);
+    *h = prngInt(HMIN,HMAX);
+    *r = prngInt(REDMIN,REDMAX);
+    *g = prngInt(GREENMIN,GREENMAX);
+    *b = prngInt(BLUEMIN, BLUEMAX);
     *op = prngFloat(OPMIN, OPMAX, PRECISION);
+
 }//genRandomArtNumbers
 
-void genRandomArt(FILE *ofp, char *id, int nShapes){
+void genRandomArt(FILE *ofp, char *id, int nShapes) {
     int x, y, rad, rx, ry, w, h, r, g, b;
     float op;
     commentSVG(ofp, ID1, "Outline SVG viewbox");
@@ -151,30 +169,38 @@ void genRandomArt(FILE *ofp, char *id, int nShapes){
     commentSVG(ofp, ID2, "Define and paint SVG view box");
     fillRectSVG(ofp, ID2, 0, 0, WINWIDTH, WINHEIGHT, 245, 245, 220, 1.0);
     commentSVG(ofp, ID2, "Draw SVG shapes");
-    for (int k=1; k<=nShapes; k++) {
+    for (int k = 1; k <= nShapes; k++) {
         // Use the function genRandomArtNumSet to generate the random numbers
         // Use the function fillCircleSVG to create the SVG shapes
-        genRandomArtNumSet(&rx, &ry, &rad, &r, &g, &b, &op);
+        int shape;
+        genRandomArtNumSet(&shape, &x, &y, &rad, &rx, &ry, &w, &h, &r, &g, &b, &op);
 
-        fillCircleSVG(ofp, id, rx, ry, rad,r, g, b, op);
+        if(shape == 0) { // Circle
+            fillCircleSVG(ofp, id, x, y, rad, r, g, b, op);
+        } else if (shape == 1) { // Rectangle
+            fillRectSVG(ofp, id, x, y, w, h, r, g, b, op);
+        } else { // shape == 2 Ellipse
+            fillEllipseSVG(ofp, id, x, y, rx, ry, r, g, b, op);
+        }
     }//for
     commentSVG(ofp, ID2, "SVG animation");
     // Use the function textSwipeAnimateSVG to create the animated text
-    textSwipeAnimateSVG(ofp, ID2, ANIMATIONTEXT, 50, 50,100, r, g, b, op);
+    textSwipeAnimateSVG(ofp, id, ANIMATIONTEXT, 40, 50, 250,255,255,255,1.0);
     closeSVG(ofp, ID1);
 }// genRandomArt
 
-FILE* createFile(char* fnam) {
-    // Create a new file.
-    // The name of the file is given in the parameter fnam
-    FILE* ofp = fopen(fnam,"w");
-    if (ofp == NULL) {
+FILE *createFile(char *fnam) {
+    FILE* newFile = fopen(fnam,"w");
+    if (newFile == NULL) {
         printf(FOPENERR);
     }
-    return ofp;
+    return newFile;
+    // Create a new file and return a real file pointer.
+    // The name of the file is given in the parameter fnam
+
 }//createFile
 
-void prologue(FILE* ofp, char* winTabTitle, char* opening, char* col) {
+void prologue(FILE *ofp, char *winTabTitle, char *opening, char *col) {
     fprintf(ofp, "%s%s\n", ID0, "<!DOCTYPE html>");
     fprintf(ofp, "%s%s\n", ID0, "<html>");
     fprintf(ofp, "%s%s\n", ID0, "<head>");
@@ -186,49 +212,59 @@ void prologue(FILE* ofp, char* winTabTitle, char* opening, char* col) {
     drawHeader1HTML5(ofp, ID1, opening, col);
 }//prologue
 
-void epilogue(FILE* ofp, char* closing, char* col) {
+void epilogue(FILE *ofp, char *closing, char *col) {
     drawParaHTML5(ofp, ID1, STRSH, col);
     drawParaHTML5(ofp, ID1, closing, col);
     fprintf(ofp, "%s%s\n", ID0, "</body>");
     fprintf(ofp, "%s%s\n", ID0, "</html>");
 }//epilogue
 
-void openSVG(FILE* ofp, char* id, int w, int h) {
+void openSVG(FILE *ofp, char *id, int w, int h) {
     fprintf(ofp, "%s<svg width=\"%d\" height=\"%d\">\n", id, w, h);
 }//openSVG
 
-void closeSVG(FILE* ofp, char* id) {
+void closeSVG(FILE *ofp, char *id) {
     fprintf(ofp, "%s</svg>\n", id);
 }//closeSVG
 
-void fillRectSVG(FILE* ofp, char* id, int x, int y, int w, int h, int r, int g, int b, float op) {
-    fprintf(ofp, "%s<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" fill=\"rgb(%d, %d, %d)\" fill-opacity=\"%.1f\"></rect>\n", id, x, y, w, h,r, g, b, op);
+void fillRectSVG(FILE *ofp, char *id, int x, int y, int w, int h, int r, int g, int b, float op) {
+    fprintf(ofp,
+            "%s<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" fill=\"rgb(%d, %d, %d)\" fill-opacity=\"%.1f\"></rect>\n",
+            id, x, y, w, h, r, g, b, op);
 }//fillRectSVG
 
-void fillCircleSVG(FILE* ofp, char* id, int cx, int cy, int rad, int r, int g, int b, float op) {
-    fprintf(ofp, "%s<circle cx=\"%d\" cy=\"%d\" r=\"%d\" fill=\"rgb(%d, %d, %d)\" fill-opacity=\"%.1f\"></circle>\n", id, cx, cy, rad, r, g, b, op);
+void fillCircleSVG(FILE *ofp, char *id, int cx, int cy, int rad, int r, int g, int b, float op) {
+    fprintf(ofp, "%s<circle cx=\"%d\" cy=\"%d\" r=\"%d\" fill=\"rgb(%d, %d, %d)\" fill-opacity=\"%.1f\"></circle>\n",
+            id, cx, cy, rad, r, g, b, op);
 }//fillCircleSVG
 
-void defAnimateSwipeSVG(FILE* ofp, char* id, char* link, int f, int t, int b, int d) {
+void fillEllipseSVG(FILE* ofp, char* id, int cx, int cy, int rx, int ry, int r, int g, int b, float op) {
+    fprintf(ofp, "%s<ellipse cx=\"%d\" cy=\"%d\" rx=\"%d\" ry=\"%d\" fill=\"rgb(%d, %d, %d)\" fill-opacity=\"%.1f\"></ellipse>\n", id, cx, cy, rx, ry, r, g, b, op);
+}//fillEllipseSVG
+
+void defAnimateSwipeSVG(FILE *ofp, char *id, char *link, int f, int t, int b, int d) {
     fprintf(ofp, "%s<defs>\n", id);
-    fprintf(ofp, "%s<animate xlink:href=\"#%s\" id=\"swipe\" attributeName=\"x\" from=\"%d\" to=\"%d\" begin=\"%ds\" dur=\"%ds\" />\n", id, link, f, t, b, d);
+    fprintf(ofp,
+            "%s<animate xlink:href=\"#%s\" id=\"swipe\" attributeName=\"x\" from=\"%d\" to=\"%d\" begin=\"%ds\" dur=\"%ds\" />\n",
+            id, link, f, t, b, d);
     fprintf(ofp, "%s</defs>\n", id);
 }//defAnimateSwipeSVG
 
-void textSwipeAnimateSVG(FILE* ofp, char* id, char* txt, int fsz, int x, int y, int r, int g, int b, float op){
+void textSwipeAnimateSVG(FILE *ofp, char *id, char *txt, int fsz, int x, int y, int r, int g, int b, float op) {
     defAnimateSwipeSVG(ofp, ID2, "defAnimateSwipeSVG", 500, 50, 0, 5);
-    fprintf(ofp, "%s<text id=\"%s\" x=\"%d\" y=\"%d\" font-size=\"%d\" fill=\"rgb(%d, %d, %d)\" fill-opacity=\"%.1f\">%s</text>\n", id, "defAnimateSwipeSVG", x, y, fsz, r, g, b, op, txt);
+    fprintf(ofp,
+            "%s<text id=\"%s\" x=\"%d\" y=\"%d\" font-size=\"%d\" fill=\"rgb(%d, %d, %d)\" fill-opacity=\"%.1f\">%s</text>\n",
+            id, "defAnimateSwipeSVG", x, y, fsz, r, g, b, op, txt);
 }//textSwipeAnimateSVG
 
-void commentSVG(FILE* ofp, char* id, char* cmt) {
+void commentSVG(FILE *ofp, char *id, char *cmt) {
     fprintf(ofp, "%s<!--%s-->\n", id, cmt);
 }//commentSVG
 
-void drawHeader1HTML5(FILE* ofp, char* id, char* text, char* col) {
+void drawHeader1HTML5(FILE *ofp, char *id, char *text, char *col) {
     fprintf(ofp, "%s<h1 style=\"color:%s\">%s</h1>\n", id, col, text);
 }//drawHeader1HTML5
 
-void drawParaHTML5(FILE* ofp, char* id, char* text, char* col) {
+void drawParaHTML5(FILE *ofp, char *id, char *text, char *col) {
     fprintf(ofp, "%s<p style=\"color:%s\">%s</p>\n", id, col, text);
 }//drawParaHTML5
-
