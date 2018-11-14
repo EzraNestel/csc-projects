8d155bb9ed2c375d9168a854bd967842f839ec44
Ezra Nestel <ezranestel@gmail.com>
2018-11-14 12:38:36 -0800

    Create Assignment 6 Part 1

--- Playground/main.c
+++ Playground/main.c
@@ -3,92 +3,18 @@
 #include <stdio.h>
 
 
-#define SIZE 5
-
-void printArray(int A[], size) {
-    for(int k = 0; k < size; k++) {
-        printf("%d\n",A[k]);
-    }
-}
-
-void initArray(int A[], int size) {
-
-    for (int k = 0; k < size; k++) {
-        A[k] = k*k;
-    }
-
-}
-
-int sumArray(int A[], int size) {
-    int sum = 0;
-    for (int k = 0; k < size; k++) {
-        sum += A[k];
-    }
-    return sum;
-}
-
-int findArraySize(int A[], int searchNumber, int size) {
-    int occurNum = 0;
-    for (int k = 0; k < size; k++) {
-        if (A[k] == searchNumber) {
-            occurNum++;
+Item dotProduct(Index len, const Vector v1, const Vector v2) {
+    int new[len];
+    for (int m = 0; m < k; m++) {
+        for (int k = 0; k < len; k++) {
+            new = v1[m] * v2[k];
         }
     }
-
-    return occurNum;
-}
-
-int findArray(int A[], int searchNumber, int B[], int size) {
-    int bIndex = 0;
-    for (int k = 0; k < size; k++) {
-        if (A[k] == searchNumber) {
-            B[bIndex] = k;
-            bIndex++;
-        }
-    }
-}
-
-int findMin(int A[], int size) {
-    int currentLow = A[0];
-    for(int k = 0; k < size; k++) {
-        if (currentLow > A[k]) {
-            currentLow = A[k];
-        }
-    }
-    return currentLow;
-}
-
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void reverse(int A[]) {
-
-    int start = SIZE-1;
-
-    for (int k = 0; k < SIZE/2; k++) {
-        swap(&A[k],&A[start]);
-        start--;
-    }
-
-
+    return new;
 }
 
 int main(void) {
-    int A[SIZE];
-    A[0] = 1;
-    A[1] = 2;
-    A[2] = 3;
-    A[3] = 1;
-    A[4] = 5;
-    int B[99];
 
-    //findArray(A, 4, B, SIZE);
-    //initArray(&A, SIZE);
-    reverse(A);
-    printArray(A, SIZE);
-    //int min = findMin(A, SIZE);
-    //printf("%d", min);
+
+    return EXIT_SUCCESS;
 }
---
+++ V00906949A6P1/main.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define VECMAX (6)
+#define VALUE (17)
+#define LOW (7)
+#define HIGH (27)
+#define SCALAR1 (5)
+#define SCALAR2 (3)
+#define SCALAR3 (2)
+typedef int Item;
+typedef int Index;
+typedef Item Vector[VECMAX];
+
+// prototypes for CSC 111 Vector API
+// functions for testing Vector API
+int main(void);
+void printVector(char *msg, Index len, const Vector v);
+int prngInt(int iLow, int iHigh);
+float prngFloat(float fLow, float fHigh, float precision);
+void setVectors(void);
+void manipulateVectors(void);
+
+// initialize or set vectors
+void setValVector(Index len, Vector v, Item val);
+void setIdxVector(Index len, Vector v);
+void setRandomVector(Index len, Vector v, Item low, Item high);
+
+// manipulate vectors
+void squareVector(Index len, Vector v);
+void addScalarVector(Index len, Vector v, Item scalar);
+void mulScalarVector(Index len, Vector v, Item scalar);
+void divScalarVector(Index len, Vector v, Item scalar);
+void addVectors(Index len, const Vector v1, const Vector v2, Vector v3);
+void mulVectors(Index len, const Vector v1, const Vector v2, Vector v3);
+
+int main(void) {
+    printf("Welcome to Linear Algebra\n");
+    setVectors();
+    manipulateVectors();
+    printf("End Linear Algebra\n");
+    return EXIT_SUCCESS;
+}//main
+
+void setVectors(void) {
+    printf("Begin setVectors\n");
+    Vector v1;
+    Vector v2;
+    Vector v3;
+    setValVector(VECMAX, v1, VALUE);
+    printVector("setValVector", VECMAX, v1);
+    setIdxVector(VECMAX, v2);
+    printVector("setIdxVector", VECMAX, v2);
+    setRandomVector(VECMAX, v3, LOW, HIGH);
+    printVector("setRandomVector", VECMAX, v3);
+    printf("End setVectors\n\n");
+}//setVectors
+
+void manipulateVectors(void) {
+    printf("Begin manipulateVectors\n");
+    Vector v1;
+    Vector v2;
+    Vector v3;
+    setRandomVector(VECMAX, v1, LOW, HIGH);
+    printVector("setRandomVector v1", VECMAX, v1);
+    setRandomVector(VECMAX, v2, LOW, HIGH);
+    printVector("setRandomVector v2", VECMAX, v2);
+    squareVector(VECMAX, v1);
+    printVector("squareVector v1", VECMAX, v1);
+    addScalarVector(VECMAX, v2, SCALAR1);
+    printVector("addScalarVector v2", VECMAX, v2);
+    mulScalarVector(VECMAX, v2, SCALAR2);
+    printVector("addScalarVector v2", VECMAX, v2);
+    divScalarVector(VECMAX, v2, SCALAR3);
+    printVector("divScalarVector v2", VECMAX, v2);
+    setRandomVector(VECMAX, v1, LOW, HIGH);
+    printVector("setRandomVector v1", VECMAX, v1);
+    setRandomVector(VECMAX, v2, LOW, HIGH);
+    printVector("setRandomVector v2", VECMAX, v2);
+    addVectors(VECMAX, v1, v2, v3);
+    printVector("addVectors v3=v1+v2", VECMAX, v3);
+    mulVectors(VECMAX, v1, v2, v3);
+    printVector("mulVectors v3=v1*v2", VECMAX, v3);
+    printf("End manipulateVectors\n\n");
+}//manipulateVectors
+
+void printVector(char *msg, Index len, const Vector v) {
+    // print all values of 'v' onto one console line
+    printf("%s ", msg);
+    for (Index k = 0; k < len; k++) printf("%d ", v[k]);
+    printf("\n");
+}//printVector
+
+void setValVector(Index len, Vector v, Item val) {
+    // initialize each component of 'v' with 'val'
+    for (Index k = 0; k < len; k++) v[k] = val;
+}//setValVector
+
+void setIdxVector(Index len, Vector v) {
+    // initialize each component of 'v' with its index
+    // replace with your code for setIdxVector
+}//setIdxVector
+
+int prngInt(int iLow, int iHigh) {
+    int mod = iHigh - iLow + 1;
+    return rand() % mod + iLow;
+}//prngInt
+
+float prngFloat(float fLow, float fHigh, float precision) {
+    int iLow = (int) (fLow * precision);
+    int iHigh = (int) (fHigh * precision);
+    int mod = iHigh - iLow + 1;
+    int irn = rand() % mod + iLow;
+    return ((float) (irn)) / precision;
+}//prngFloat
+
+void setRandomVector(Index len, Vector v, Item low, Item high) {
+    // initialize each component of 'v' with a pseudo
+    // random numbers in the range of 'low' to 'high'
+    for (Index k = 0; k < len; k++) v[k] = prngInt(low, high);
+}//setRandomVector
+
+void addScalarVector(Index len, Vector v, Item scalar) {
+    // add 'scalar' to each component of 'v’
+    // replace with your code for addScalarVector
+}//addScalarVector
+
+void mulScalarVector(Index len, Vector v, Item scalar) {
+    // multiply each component of 'v' by 'scalar'
+    // replace with your code for mulScalarVector
+}//mulScalarVector
+
+void divScalarVector(Index len, Vector v, Item scalar) {
+    // multiply each component of 'v' by 'scalar'
+    // replace with your code for divScalarVector
+}//divScalarVector
+
+void squareVector(Index len, Vector v) {
+    // v[k] = v[k] * v[k]
+    // replace with your code for squareVector
+}//squareVector
+
+void addVectors(Index len, const Vector v1, const Vector v2, Vector v3) {
+    // v3[k] = v1[k] + v2[k]
+    // replace with your code for addVectors
+}//addVectors
+
+void mulVectors(Index len, const Vector v1, const Vector v2, Vector v3) {
+    // v3[k] = v1[k] * v2[k]
+    // replace with your code for mulVectors
+}//mulVectors
