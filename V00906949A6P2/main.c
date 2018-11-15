871fa46ffe54ec916809f6a6bb385c0fd7b41916
Ezra Nestel <ezranestel@gmail.com>
2018-11-14 19:42:54 -0800

    Added Problem 2 (Assignment 6)

---
+++ V00906949A6P2/main.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define VECMAX (6)
+#define LOW (7)
+#define HIGH (27)
+#define SPECINDEX (3)
+#define WILLNOTBEFOUND (-77)
+#define NOTFOUND (-1)
+typedef int Item;
+typedef int Index;
+typedef Item Vector[VECMAX];
+
+// prototypes for CSC 111 Vector API
+// functions for testing Vector API
+int main(void);
+void printVector(char* msg, Index len, const Vector v);
+void printValueFound(bool found, Item val, Item fidx);
+int prngInt(int iLow, int iHigh);
+float prngFloat(float fLow, float fHigh, float precision);
+void calculateFindVectorValues(void);
+
+// initialize or set vectors
+void setRandomVector(Index len, Vector v, Item low, Item high);
+
+// calculate for vectors and find values in vectors
+Item sumVector(Index len, const Vector v);
+Item dotProductVectors(Index len, const Vector v1, const Vector v2);
+Item findMaxInVector(Index len, const Vector v);
+Item findMinInVector(Index len, const Vector v);
+bool findValueInVector(Index len, const Vector v, Item val, Index* fidx);
+
+int main(void) {
+    printf("Welcome to Linear Algebra\n");
+    calculateFindVectorValues();
+    printf("End Linear Algebra\n");
+    return EXIT_SUCCESS;
+}//main
+
+void calculateFindVectorValues(void){
+    printf("Begin calculateFindVectorValues\n");
+    Vector v1;
+    Vector v2;
+    setRandomVector(VECMAX, v1, LOW, HIGH);
+    printVector("setRandomVector v1", VECMAX, v1);
+    setRandomVector(VECMAX, v2, LOW, HIGH);
+    printVector("setRandomVector v2", VECMAX, v2);
+    Item dot = dotProductVectors(VECMAX, v1, v2);
+    printf("dotProductVectors = %d\n", dot);
+    Item sum = sumVector(VECMAX, v1);
+    printf("sumVector = %d\n", sum);
+    Item max = findMaxInVector(VECMAX, v1);
+    printf("findMaxInVector = %d\n", max);
+    Item min = findMinInVector(VECMAX, v1);
+    printf("findMinInVector = %d\n", min);
+    Index fidx = NOTFOUND;
+    Item val = v1[SPECINDEX];
+    bool found = findValueInVector(VECMAX, v1, val, &fidx);
+    printValueFound(found, val, fidx);
+    fidx = NOTFOUND;
+    val = WILLNOTBEFOUND;
+    found = findValueInVector(VECMAX, v1, val, &fidx);
+    printValueFound(found, val, fidx);
+    printf("End calculateFindVectorValues\n\n");
+}//calculateFindVectorValues
+
+void printValueFound(bool found, Item val, Item fidx) {
+    if (found) printf("Value %d found in Vector v1 at index %d\n", val, fidx);
+    else printf("Value %d not found in Vector v1\n", val);
+}//printValueFound
+
+void printVector(char* msg, Index len, const Vector v) {
+    // print all values of 'v' onto one console line
+    printf("%s ", msg);
+    for (Index k = 0; k < len; k++) printf("%d ", v[k]);
+    printf("\n");
+}//printVector
+
+int prngInt(int iLow, int iHigh) {
+    int mod = iHigh - iLow + 1;
+    return rand() % mod + iLow;
+}//prngInt
+
+float prngFloat(float fLow, float fHigh, float precision) {
+    int iLow = (int)(fLow*precision);
+    int iHigh = (int)(fHigh*precision);
+    int mod = iHigh - iLow + 1;
+    int irn = rand()%mod + iLow;
+    return ((float)(irn)) / precision;
+}//prngFloat
+
+void setRandomVector(Index len, Vector v, Item low, Item high){
+    // initialize each component of 'v' with a pseudo
+    // random numbers in the range of 'low' to 'high'
+    for (Index k=0; k<len; k++) v[k] = prngInt(low, high);
+}//setRandomVector
+
+Item sumVector(Index len, const Vector v){
+    // return the sum of all components of 'v’
+    // replace with your code for sumVector
+    return -1;
+}//sumVector
+
+Item dotProductVectors(Index len, const Vector v1, const Vector v2) {
+    // return dot product of v1 and v2
+    // Ex: dot product of [1 7 6 2 4] and [3 1 2 1 4] is 40
+    // replace with your code for dotProduct
+    return -1;
+}//dotProductVectors
+
+Item findMinInVector(Index len, const Vector v) {
+    // find and return min array element
+    // Ex: min of [1 7 6 2 4] is 1
+    // replace with your code for findMinInVector
+    return -1;
+}//findMinInVector
+
+Item findMaxInVector(Index len, const Vector v) {
+    // find and return min array element
+    // Ex: max of [1 7 6 2 4] is 7
+    // replace with your code for findMaxInVector
+    return -1;
+}//findMaxInVector
+
+bool findValueInVector(Index len, const Vector v, Item val, Index* fidx){
+    // replace with your code for findValueInVector
+    return false;
+}//findValueInVector
